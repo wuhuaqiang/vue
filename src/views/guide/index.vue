3527040f8af0c112d7d7eaffd03700dec0981151
吴华强 <609676374@qq.com>
2019-10-22 08:59:47 +0800

    init

---
+++ src/views/guide/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Guide',
+  data() {
+    return {
+      driver: null
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
